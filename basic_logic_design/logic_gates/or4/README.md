9f16056f75b4b1d906a4207d3cda70aff3a3dca3
dnlvcnt-lab <dandvrodrigo@gmail.com>
2022-08-12 19:46:05 +0200

    add or4 logic gate

---
+++ basic_logic_design/logic_gates/or4/README.md
@@ -0,0 +1,27 @@
+# OR4 Gate
+
+## Definition
+The OR gate is a basic digital logic gate that implements logical disjunction (∨) from mathematical logic. 
+
+### Truth Table
+|a | b | c | d | e = f(a,b,c,d)|
+|:---:|:---:|:---:|:---:|:---:|
+|0| 0| 0| 0| 0|
+|0| 0| 0| 1| 1|
+|0| 0| 1| 0| 1|
+|0| 0| 1| 1| 1|
+|0| 1| 0| 0| 1|
+|0| 1| 0| 1| 1|
+|0| 1| 1| 0| 1|
+|0| 1| 1| 1| 1|
+|1| 0| 0| 0| 1|
+|1| 0| 0| 1| 1|
+|1| 0| 1| 0| 1|
+|1| 0| 1| 1| 1|
+|1| 1| 0| 0| 1|
+|1| 1| 0| 1| 1|
+|1| 1| 1| 0| 1|
+|1| 1| 1| 1| 1|
+
+## Test Bench
+![or4 tb](./or4_tb.png "OR4 Test Bench")
---
+++ basic_logic_design/logic_gates/or4/or4_tb.v
@@ -0,0 +1,128 @@
+//-------------------------------------------------------------------
+//-- or4_tb.v
+//-- Testbench
+//-------------------------------------------------------------------
+//-- Daniel Rodrigo
+//-- GPL license
+//-------------------------------------------------------------------
+
+`default_nettype none
+`define DUMPSTR(x) `"x.vcd`"
+`timescale 1 ns/10 ps  // time-unit = 1 ns, precision = 10 ps
+
+module or4_tb;
+
+    reg x0, x1, x2, x3;
+    wire z0;
+
+    //-- Simulation time: 20ns (20 * 1ns)
+    localparam DURATION = 20;  
+
+    or4 UUT (.x0(x0), .x1(x1), .x2(x2), .x3(x3), .z0(z0));
+    
+    initial begin
+	
+		//-- File were to store the simulation results
+		$dumpfile(`DUMPSTR(`VCD_OUTPUT));
+		$dumpvars(0, or4_tb);
+  
+        x0 = 0;
+		x1 = 0;
+        x2 = 0;
+        x3 = 0;
+        #DURATION;
+
+        x0 = 0;
+		x1 = 0;
+        x2 = 0;
+        x3 = 1;
+        #DURATION;
+
+        x0 = 0;
+		x1 = 0;
+        x2 = 1;
+        x3 = 0;
+        #DURATION;
+
+        x0 = 0;
+		x1 = 0;
+        x2 = 1;
+        x3 = 1;
+        #DURATION;
+		
+        x0 = 0;
+		x1 = 1;
+        x2 = 0;
+        x3 = 0;
+        #DURATION;
+
+        x0 = 0;
+		x1 = 1;
+        x2 = 0;
+        x3 = 1;
+        #DURATION;
+
+        x0 = 0;
+		x1 = 1;
+        x2 = 1;
+        x3 = 0;
+        #DURATION;
+
+        x0 = 0;
+		x1 = 1;
+        x2 = 1;
+        x3 = 1;
+        #DURATION;
+
+        x0 = 1;
+		x1 = 0;
+        x2 = 0;
+        x3 = 0;
+        #DURATION;
+
+        x0 = 1;
+		x1 = 0;
+        x2 = 0;
+        x3 = 1;
+        #DURATION;
+
+        x0 = 1;
+		x1 = 0;
+        x2 = 1;
+        x3 = 0;
+        #DURATION;
+
+        x0 = 1;
+		x1 = 0;
+        x2 = 1;
+        x3 = 1;
+        #DURATION;
+		
+        x0 = 1;
+		x1 = 1;
+        x2 = 0;
+        x3 = 0;
+        #DURATION;
+
+        x0 = 1;
+		x1 = 1;
+        x2 = 0;
+        x3 = 1;
+        #DURATION;
+
+        x0 = 1;
+		x1 = 1;
+        x2 = 1;
+        x3 = 0;
+        #DURATION;
+
+        x0 = 1;
+		x1 = 1;
+        x2 = 1;
+        x3 = 1;
+        #DURATION;
+		
+		$display("End of simulation");
+		$finish;
+    end
+endmodule
